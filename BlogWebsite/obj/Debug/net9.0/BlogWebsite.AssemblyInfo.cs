bcde17728b5c33c17dc1fcf6cbd083f7111029fa
MD-Mustak <mustakarman560@gmail.com>
2025-08-20 10:29:46 +0530

    duplicates remove

--- BlogWebsite/obj/Debug/net9.0/BlogWebsite.AssemblyInfo.cs
+++ BlogWebsite/obj/Debug/net9.0/BlogWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4daa428a3871502af8b0f9e4415713c50f882ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BlogWebsite.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
