7c7d1c1970e4d655bb299971f8f50f362c1c7855
MD-Mustak <mustakarman560@gmail.com>
2025-08-31 17:14:28 +0530

    Done

--- obj/Debug/net9.0/BlogWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlogWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6bda5651c3b59da9df46ed7f92b30765f998d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd292f229b54335cb402ed9bcfb62cfc4184ac00")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
